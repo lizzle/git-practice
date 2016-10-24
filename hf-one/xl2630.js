76527b78a1598c58a6110dc6cffc83018dbe2ea8
xl2630 <Xinyi@liuxinyis-MacBook.local>
2016-10-23 23:08:04 -0400

    xl2630
    Javascript practice

---
+++ hf-one/xl2630.js
@@ -0,0 +1,17 @@
+if (xinyi>8) {
+  console.log("you have a long name");
+}
+else {
+  console.log("you have a short name");
+}
+
+var greeting=function(name)
+{
+console.log("great to see you,"+""+name);};
+greeting("Xinyi");
+
+var perimeterBox=function(length,width){
+    return length+length+width+width;
+};
+perimeterBox(3,4);
+console.log(14);
